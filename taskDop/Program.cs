97b4c23ae7c664b74a16b4590237e3dd28f6b85e
Natalya <alkinatalya@yandex.com>
2022-11-22 19:34:42 +0300

    add taskDop

---
+++ taskDop/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Дополнительно. Дан массив. Найдите MEX массива. MEX (Minimum EXcluded)
+ - минимальный отсутствующий элемент. */
+
+int GetMEX(int[] values)
+{
+    for (int mex = 0; mex < values.Length; mex++)
+    {
+        bool notFound = true;
+        for (int i = 0; i < values.Length; i++)
+        {
+            if (values[i] == mex)
+            {
+                notFound = false;
+                break;
+            }
+        }
+        if (notFound)
+        {
+            return mex;
+        }
+    }
+    return values.Length;
+}
+int[] GenerateArray(int length, int minRandom, int maxRandom)
+{
+    Random rnd = new Random();
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minRandom, maxRandom + 1);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + "\t");
+    }
+    System.Console.WriteLine();
+}
+int[] array = GenerateArray(14, 0, 10);
+PrintArray(array);
+int mex = GetMEX(array);
+Console.WriteLine($"MEX (Minimum EXcluded) - минимальный отсутствующий элемент в массиве: {mex}");
